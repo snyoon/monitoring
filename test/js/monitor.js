81eda2e3082a07e2fe4925922d6b22a6743e26a6
snyoon <justinyoon23@gmail.com>
2017-12-05 14:33:27 +0900

    Bug Fix UTil

--- test/js/monitor.js
+++ test/js/monitor.js
@@ -2420,8 +2420,8 @@ function statViewPage(object, divID){
     var utilData = new vis.DataSet();
     var utilGroup = new vis.DataSet();
 
-    visArrayDatasetAdd(utilObjDA, utilGroup, "DA");
-    visArrayDatasetAdd(utilObjWB, utilGroup, "WB");
+    visArrayDatasetAdd(utilObjDA, utilData, "DA");
+    visArrayDatasetAdd(utilObjWB, utilData, "WB");
 
     utilGroup.add({
         id:"DA",
